5d6d9da489e63ec0e06047ea29cd95397318e9e8
Gouderg <victor.illien974@gmail.com>
2025-02-28 19:34:44 +0400

    Rewrite unit tests to simplify

--- tests/vector_basics.cpp
+++ tests/vector_basics.cpp
@@ -14,216 +14,153 @@ TEST_CASE("Vector constructor", "[operator+]") {
 }
 
 
-TEST_CASE("Vector add v + v2", "[operator+]") {
+TEST_CASE("Vector addition operator", "[operator+]") {
     Vector v1 = Vector(1, 2);
     Vector v2 = Vector(1, 2);
-    Vector v = v1 + v2;
-
-
-    REQUIRE(v.getX() == 2.0);
-    REQUIRE(v.getY() == 4.0);   
-    REQUIRE(v.getZ() == 0.0);   
-}
-
-TEST_CASE("Vector add v + n", "[operator+]") {
-    Vector v1 = Vector(1, 2);
     double n = 3.0;
-    Vector v = v1 + n;
+    
+    SECTION("v + v2") {
+        Vector v = v1 + v2;
+        REQUIRE((v.getX() == 2.0 && v.getY() == 4.0 && v.getZ() == 0.0));
+    }
 
+    SECTION("v + n") {
+        Vector v = v1 + n;
+        REQUIRE((v.getX() == 4.0 && v.getY() == 5.0 && v.getZ() == 3.0));
+    }
 
-    REQUIRE(v.getX() == 4.0);
-    REQUIRE(v.getY() == 5.0);   
-    REQUIRE(v.getZ() == 3.0);   
+    SECTION("n + v") {
+        Vector v = n + v2;
+        REQUIRE((v.getX() == 4.0 && v.getY() == 5.0 && v.getZ() == 3.0));
+    }
 }
 
-TEST_CASE("Vector add n + v", "[operator+]") {
-    Vector v1 = Vector(1, 2);
-    double n = 3.0;
-    Vector v = n + v1;
-
-    REQUIRE(v.getX() == 4.0);
-    REQUIRE(v.getY() == 5.0);   
-    REQUIRE(v.getZ() == 3.0);
-}
-
-TEST_CASE("Vector v += v2", "[operator+=]") {
+TEST_CASE("Vector addition operator", "[operator+=]") {
     Vector v = Vector(1, 2);
     Vector v2 = Vector(1, 2);
-    v += v2;
-
-    REQUIRE(v.getX() == 2.0);
-    REQUIRE(v.getY() == 4.0);   
-    REQUIRE(v.getZ() == 0.0);   
-}
-
-TEST_CASE("Vector v += n", "[operator+=]") {
-    Vector v = Vector(1, 2);
     double n = 3.0;
-    v += n;
+    
+    SECTION("v += v2") {
+        v += v2;
+        REQUIRE((v.getX() == 2.0 && v.getY() == 4.0 && v.getZ() == 0.0));
+    }
 
-    REQUIRE(v.getX() == 4.0);
-    REQUIRE(v.getY() == 5.0);   
-    REQUIRE(v.getZ() == 3.0);
+    SECTION("v + n") {
+        v += n;
+        REQUIRE((v.getX() == 4.0 && v.getY() == 5.0 && v.getZ() == 3.0));
+    }
 }
 
-TEST_CASE("Vector add v - v2", "[operator-]") {
+TEST_CASE("Vector soustraction operator", "[operator-]") {
     Vector v1 = Vector(1, 2);
-    Vector v2 = Vector(2, 3);
-    Vector v = v1 - v2;
-
-
-    REQUIRE(v.getX() == -1.0);
-    REQUIRE(v.getY() == -1.0);   
-    REQUIRE(v.getZ() == 0.0);   
-}
-
-TEST_CASE("Vector add v - n", "[operator-]") {
-    Vector v1 = Vector(1, 2);
+    Vector v2 = Vector(1, 2);
     double n = 3.0;
-    Vector v = v1 - n;
+    
+    SECTION("v - v2") {
+        Vector v = v1 - v2;
+        REQUIRE((v.getX() == 0.0 && v.getY() == 0.0 && v.getZ() == 0.0));
+    }
 
+    SECTION("v - n") {
+        Vector v = v1 - n;
+        REQUIRE((v.getX() == -2.0 && v.getY() == -1.0 && v.getZ() == -3.0));
+    }
 
-    REQUIRE(v.getX() == -2.0);
-    REQUIRE(v.getY() == -1.0);   
-    REQUIRE(v.getZ() == -3.0);   
+    SECTION("n - v") {
+        Vector v = n - v2;
+        REQUIRE((v.getX() == 2.0 && v.getY() == 1.0 && v.getZ() == 3.0));
+    }
 }
 
-TEST_CASE("Vector add n - v", "[operator-]") {
-    Vector v1 = Vector(1, 2);
-    double n = 3.0;
-    Vector v = n - v1;
-
-    REQUIRE(v.getX() == 2.0);
-    REQUIRE(v.getY() == 1.0);   
-    REQUIRE(v.getZ() == 3.0);
-}
-
-TEST_CASE("Vector v -= v2", "[operator-=]") {
+TEST_CASE("Vector soustraction operator", "[operator-=]") {
     Vector v = Vector(1, 2);
     Vector v2 = Vector(1, 2);
-    v -= v2;
-
-    REQUIRE(v.getX() == 0.0);
-    REQUIRE(v.getY() == 0.0);   
-    REQUIRE(v.getZ() == 0.0);   
-}
-
-TEST_CASE("Vector v -= n", "[operator-=]") {
-    Vector v = Vector(1, 2);
     double n = 3.0;
-    v -= n;
+    
+    SECTION("v -= v2") {
+        v -= v2;
+        REQUIRE((v.getX() == 0.0 && v.getY() == 0.0 && v.getZ() == 0.0));
+    }
 
-    REQUIRE(v.getX() == -2.0);
-    REQUIRE(v.getY() == -1.0);   
-    REQUIRE(v.getZ() == -3.0);
+    SECTION("v - n") {
+        v -= n;
+        REQUIRE((v.getX() == -2.0 && v.getY() == -1.0 && v.getZ() == -3.0));
+    }
 }
 
-TEST_CASE("Vector add v * v2", "[operator*]") {
+
+TEST_CASE("Vector multiplication operator", "[operator*]") {
     Vector v1 = Vector(1, 2);
-    Vector v2 = Vector(2, 3);
-    Vector v = v1 * v2;
-
-
-    REQUIRE(v.getX() == 2.0);
-    REQUIRE(v.getY() == 6.0);   
-    REQUIRE(v.getZ() == 0.0);   
-}
-
-TEST_CASE("Vector add v * n", "[operator*]") {
-    Vector v1 = Vector(1, 2);
+    Vector v2 = Vector(1, 2);
     double n = 3.0;
-    Vector v = v1 * n;
+    
+    SECTION("v * v2") {
+        Vector v = v1 * v2;
+        REQUIRE((v.getX() == 1.0 && v.getY() == 4.0 && v.getZ() == 0.0));
+    }
 
+    SECTION("v * n") {
+        Vector v = v1 * n;
+        REQUIRE((v.getX() == 3.0 && v.getY() == 6.0 && v.getZ() == 0.0));
+    }
 
-    REQUIRE(v.getX() == 3.0);
-    REQUIRE(v.getY() == 6.0);   
-    REQUIRE(v.getZ() == 0.0);   
+    SECTION("n / v") {
+        Vector v = n * v2;
+        REQUIRE((v.getX() == 3.0 && v.getY() == 6.0 && v.getZ() == 0.0));
+    }
 }
 
-TEST_CASE("Vector add n * v", "[operator*]") {
-    Vector v1 = Vector(1, 2);
-    double n = 3.0;
-    Vector v = n * v1;
-
-    REQUIRE(v.getX() == 3.0);
-    REQUIRE(v.getY() == 6.0);   
-    REQUIRE(v.getZ() == 0.0);
-}
-
-TEST_CASE("Vector v *= v2", "[operator*=]") {
+TEST_CASE("Vector multiplication operator", "[operator*=]") {
     Vector v = Vector(1, 2);
     Vector v2 = Vector(1, 2);
-    v *= v2;
-
-    REQUIRE(v.getX() == 1.0);
-    REQUIRE(v.getY() == 4.0);   
-    REQUIRE(v.getZ() == 0.0);   
-}
-
-TEST_CASE("Vector v *= n", "[operator*=]") {
-    Vector v = Vector(1, 2);
     double n = 3.0;
-    v *= n;
+    
+    SECTION("v *= v2") {
+        v *= v2;
+        REQUIRE((v.getX() == 1.0 && v.getY() == 4.0 && v.getZ() == 0.0));
+    }
 
-    REQUIRE(v.getX() == 3.0);
-    REQUIRE(v.getY() == 6.0);   
-    REQUIRE(v.getZ() == 0.0);
+    SECTION("v * n") {
+        v *= n;
+        REQUIRE((v.getX() == 3.0 && v.getY() == 6.0 && v.getZ() == 0.0));
+    }
 }
 
-TEST_CASE("Vector add v / v2", "[operator/]") {
-    Vector v1 = Vector(1, 2, 1);
-    Vector v2 = Vector(2, 4, 1);
-    Vector v = v1 / v2;
-
-
-    REQUIRE(v.getX() == 0.5);
-    REQUIRE(v.getY() == 0.5);   
-    REQUIRE(v.getZ() == 1.0);   
-}
-
-TEST_CASE("Vector add v / n", "[operator/]") {
-    Vector v1 = Vector(1, 2);
-    double n = 10.0;
-    Vector v = v1 / n;
-
-
-    REQUIRE(v.getX() == 0.1);
-    REQUIRE(v.getY() == 0.2);   
-    REQUIRE(v.getZ() == 0.0);   
-}
-
-TEST_CASE("Vector add n / v", "[operator/]") {
-    Vector v1 = Vector(1, 6, 3);
+TEST_CASE("Vector division operator", "[operator/]") {
+    Vector v1 = Vector(3, 6);
+    Vector v2 = Vector(6, 12, 3);
     double n = 3.0;
-    Vector v = n / v1;
+    
+    SECTION("v / v2") {
+        Vector v = v1 / v2;
+        REQUIRE((v.getX() == 0.5 && v.getY() == 0.5 && v.getZ() == 0.0));
+    }
 
-    REQUIRE(v.getX() == 3.0);
-    REQUIRE(v.getY() == 0.5);   
-    REQUIRE(v.getZ() == 1.0);
+    SECTION("v / n") {
+        Vector v = v1 / n;
+        REQUIRE((v.getX() == 1.0 && v.getY() == 2.0 && v.getZ() == 0.0));
+    }
+
+    SECTION("n / v") {
+        Vector v = n / v2;
+        REQUIRE((v.getX() == 0.5 && v.getY() == 0.25 && v.getZ() == 1.0));
+    }
 }
 
-TEST_CASE("Vector v /= v2", "[operator/=]") {
-    Vector v = Vector(1, 2);
+TEST_CASE("Vector division operator", "[operator/=]") {
+    Vector v = Vector(3, 6);
     Vector v2 = Vector(1, 2, 3);
-    v /= v2;
-
-    REQUIRE(v.getX() == 1.0);
-    REQUIRE(v.getY() == 1.0);   
-    REQUIRE(v.getZ() == 0.0);   
-}
-
-TEST_CASE("Vector v /= n", "[operator/=]") {
-    Vector v = Vector(3, 6);
     double n = 3.0;
-    v /= n;
+    
+    SECTION("v /= v2") {
+        v /= v2;
+        REQUIRE((v.getX() == 3.0 && v.getY() == 3.0 && v.getZ() == 0.0));
+    }
 
-    REQUIRE(v.getX() == 1.0);
-    REQUIRE(v.getY() == 2.0);   
-    REQUIRE(v.getZ() == 0.0);
-}
-
-TEST_CASE("Vector Try to divide by 0", "[operator/=]") {
-    Vector v = Vector(3, 6);
+    SECTION("v /= n") {
+        v /= n;
+        REQUIRE((v.getX() == 1.0 && v.getY() == 2.0 && v.getZ() == 0.0));
+    }
 
     SECTION("Division by zero causes program to abort") {
         REQUIRE_THROWS_AS(v /= 0.0, std::invalid_argument);
